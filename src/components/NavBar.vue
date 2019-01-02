cb21af747eec3b2afa883bcf7ac855602fde2b31
Nick Moore <nickmoore_1@live.com>
2019-01-02 16:23:38 -0500

    initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,6 @@
+<template>
+  <div id="nav" class="w-full bg-my-navy-1 h-12 font-sans font-thin text-my-teal-1 text-2xl pl-4 pr-4 pt-2">
+    <router-link to="/" class="text-my-teal-1">Home</router-link>|
+    <router-link to="/about" class="text-my-teal-1">About</router-link>
+  </div>
+</template>
