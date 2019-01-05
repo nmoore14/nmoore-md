746eb4531f3f73f311486d78524f405a7d88861c
Nick Moore <nickmoore_1@live.com>
2019-01-05 16:35:53 -0500

    Adding the marquee component to the home view

---
+++ src/components/HomeMarquee.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id='main-marquee' class='flex w-screen h-12 bg-my-teal-2 items-center'>
+    <marquee class='font-sans font-thin text-2xl text-my-navy-1'>Markdown Document Helper - More updates coming soon</marquee>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <welcome></welcome>
+    <home-marquee></home-marquee>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Welcome from '@/components/Welcome.vue'
+import HomeMarquee from '@/components/HomeMarquee.vue'
 
 export default {
   name: 'home',
   components: {
-    Welcome
+    Welcome,
+    HomeMarquee
   }
 }
 </script>
