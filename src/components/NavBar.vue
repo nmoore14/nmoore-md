8330e4ac306ba04cbf73ed2471b80d30738e6dda
Nick Moore <nickmoore_1@live.com>
2019-01-05 18:55:09 -0500

    Changing the path back to the original setting

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,7 @@
     id="nav"
     class="w-full bg-my-navy-1 h-12 font-sans font-thin text-my-teal-1 text-2xl pl-4 pr-4 pt-2 shadow-md fixed"
   >
-    <router-link to="/home" class="text-my-teal-1 no-underline hover:font-normal">&lt;Home&gt;&nbsp;</router-link>
+    <router-link to="/" class="text-my-teal-1 no-underline hover:font-normal">&lt;Home&gt;&nbsp;</router-link>
     <router-link
       to="/about"
       class="text-my-teal-1 no-underline hover:font-normal"
